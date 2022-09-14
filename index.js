9fc9512c4880236a1242002322a6f21abebf1f71
Sasa Milosevic <sasmilo@gmail.com>
2022-09-14 11:01:56 +0200

    Add endpoints for GET and POST requests

--- index.js
+++ index.js
@@ -3,17 +3,40 @@ import Products from "./products.js"
 
 const app = Express()
 const port = 3000
+app.use(Express.json())
+app.use(Express.urlencoded({ extended: true }))
 
-
-app.get("/", (req, res) => {
-    // res.send("Hello World!")
-    res.json(Products)
-})
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)})
 
 
-app.post("/", (req, res) => {})
+app.get("/", (req, res) => {
+    res.send('Welcome to the API')
+})
+
+    
+app.get("/products", (req, res) => {
+    res.json(Products)
+    res.send(req.params)
+})
+
+
+app.get("/products/:id", (req, res) => {
+    res.json(Products.find((product) => product.id === Number(req.params.id)))
+    res.send(req.params)
+    // res.json(Products)
+})
+
+
+app.post("/add", (req, res) => {
+    // res.send(req.body)
+    res.json(Products.push(req.body))
+     res.send(Products)
+
+
+})
+
+
 app.put("/", (req, res) => {})
 app.delete("/", (req, res) => {})
 
