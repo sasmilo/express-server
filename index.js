2634d79b70dc56739d7388d50bd603379aa2ac84
Sasa Milosevic <sasmilo@gmail.com>
2022-09-14 13:25:37 +0200

    Add dependencies

--- index.js
+++ index.js
@@ -9,34 +9,34 @@ app.use(Express.urlencoded({ extended: true }))
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)})
 
-
+// Get the first page
 app.get("/", (req, res) => {
     res.send('Welcome to the API')
 })
 
-    
+// Get the list of all products   
 app.get("/products", (req, res) => {
     res.json(Products)
-    res.send(req.params)
+    // res.send(req.params)
 })
 
-
+// Get one product by id
 app.get("/products/:id", (req, res) => {
     res.json(Products.find((product) => product.id === Number(req.params.id)))
-    res.send(req.params)
+    // res.send(req.params)
     // res.json(Products)
 })
 
-
+// Create a new product
 app.post("/add", (req, res) => {
     // res.send(req.body)
     res.json(Products.push(req.body))
-     res.send(Products)
+    //  res.send(Products)
 
 
 })
 
-
+// Update a product by id and status property
 app.put("/updatestatus/:id", (req, res) => {
     const product = Products.find((product) => product.id === Number(req.params.id))
     if (product) {
@@ -47,13 +47,13 @@ app.put("/updatestatus/:id", (req, res) => {
         throw new Error("Product not found")
     }
 
-    res.json(Products)
+    // res.json(Products)
     // res.send(req.params)
     // res.json(Products)
 
 })
 
-
+// Delete a product by id
 app.delete("/delete/:id", (req, res) => {
     const product = Products.find((product) => product.id === Number(req.params.id))
     if (product) {
